4ef5813f3f641fcdba90920de3bcb894e4fbf7f5
анила алафеев <bear063.malafeev@yandex.ru>
2024-03-06 22:39:11 +0400

    Основное приложение готово

--- main/views.py
+++ main/views.py
@@ -3,14 +3,18 @@ from django.http import HttpResponse
 
 def index(request):
     context: dict = {
-        "title": "Home",
-        "content": "Главная страница магазина - HOME",
-        "list": ["first", "second"],
-        "dict": {"first": 1},
-        "is_aut": False
+        "title": "Home - главная",
+        "content": "Магазин мебели HOME"
+
     }
 
     return render(request,"main/index.html", context)
 
 def about(request):
-    return HttpResponse("About page")
+    context: dict = {
+        "title": "Home - О нас",
+        "content": "О нас",
+        "text_on_page": "Текст о том почему этот магазин такой классный, и какой хороший товар"
+    }
+
+    return render(request, "main/about.html", context)
