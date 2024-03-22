33a8d2b1807408d7aad1217c449d0587e3e90f06
анила алафеев <bear063.malafeev@yandex.ru>
2024-03-22 21:58:27 +0400

    Доделано: Админ-панель. Работающий вариант

--- goods/models.py
+++ goods/models.py
@@ -13,6 +13,10 @@ class Categories(models.Model):
         verbose_name_plural = 'Категории'
 
 
+    def __str__(self):
+        return self.name
+
+
 class Products(models.Model):
     name = models.CharField(max_length=150, unique=True, null=True, verbose_name='Название')
     slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
@@ -27,3 +31,5 @@ class Products(models.Model):
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
 
+    def __str__(self):
+        return f'{self.name} Количество - {self.qantity}'
